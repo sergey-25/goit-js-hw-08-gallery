2b74f8c2f3e335edd0ab9aecc1c76ba0969c0d87
Kurylenko Serhii <sergey25111988@gmail.com>
2021-04-21 08:00:38 +0300

    hm8

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,12 @@
 import galleryItems from '../gallery-items.js';
-//Referrals of elements
+
 const elemRefs = {
   gallery: document.querySelector('.js-gallery'),
   closeBtn: document.querySelector('.lightbox__button'),
   modal: document.querySelector('.js-lightbox'),
   modalImage: document.querySelector('.lightbox__image'),
 };
-// Create galery items
-// At the output, we get a string with items of gallery markup
+
 const galleryMarkup = galleryItems
   .map((item, i) => {
     return `<li class="gallery__item">
@@ -26,26 +25,26 @@ const galleryMarkup = galleryItems
     </li>`;
   })
   .join('');
-// Adding items to the markup
+
 elemRefs.gallery.innerHTML = galleryMarkup;
-// Adding listeners
+
 elemRefs.gallery.addEventListener('click', event => {
-  // Disable the following image link
+  
   event.preventDefault();
   if (event.target.tagName !== 'IMG') return;
-  //Adding class 'is-open' to modal window
+  
   elemRefs.modal.classList.add('is-open');
-  // Adding an image id to the modal dataset from the gallery item where the event happened
+  
   elemRefs.modalImage.dataset.ind = event.target.dataset.index;
   elemRefs.modalImage.alt =
     galleryItems[+elemRefs.modalImage.dataset.ind].description;
   elemRefs.modalImage.src =
     galleryItems[+elemRefs.modalImage.dataset.ind].original;
-  // Listener on the close button for close modal
+  
   elemRefs.closeBtn.addEventListener('click', () => {
     closeLightBox();
   });
-  // Listener on the modal window for close modal
+ 
   elemRefs.modal.addEventListener('click', event => {
     if (event.target.tagName === 'IMG') {
       return;
